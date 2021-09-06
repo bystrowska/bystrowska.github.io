462affbba7ca7d236d2d419e39d87b9e900c5c2b
Paulina Bystrowska <50452580+bystrowska@users.noreply.github.com>
2021-09-06 13:57:41 +0200

    add font and typewriter plus fade in effect

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,6 @@
 import * as React from 'react'
+import "@fontsource/source-code-pro"
+import "@fontsource/source-code-pro/600.css"
 import { body } from './layout.module.scss'
 
 const Layout = ({ pageTitle, children }) => {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import * as React from "react"
 import Layout from '../components/layout'
+import { h1, subtitle } from './index.module.scss'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Paulina's Page">
       <h1>Hi, I'm Paulina</h1>
-      <p>A final year BSc Computer Science student at the University of Leeds.</p>
+      <p className={subtitle}>A final year BSc Computer Science student at the University of Leeds.</p>
     </Layout>
   )
 }
